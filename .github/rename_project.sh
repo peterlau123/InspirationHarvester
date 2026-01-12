203a13551642c0f562e0ae4b6816fd0aad20395d
peterlau123 <peterlau123@users.noreply.github.com>
2026-01-12 15:48:04 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="peterlau123"
+original_name="inspirationharvester"
+original_urlname="InspirationHarvester"
+original_description="Awesome inspirationharvester created by peterlau123"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv inspirationharvester $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
---
+++ inspirationharvester/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for inspirationharvester."""
+
+from inspirationharvester.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
